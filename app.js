ff5255cdc6d2d2e63b80916d1e6ade81d253b186
yedio <yediobjet@gmail.com>
2021-08-18 16:08:13 +0900

    Change Color

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
 
 canvas.width = document.getElementsByClassName("canvas")[0].offsetWidth;
 canvas.height = document.getElementsByClassName("canvas")[0].offsetHeight;
@@ -31,10 +32,10 @@ function onMouseMove(event){
     }
 }
 
-function onMouseDown(event){
-    painting= true;
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
 }
-
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
@@ -43,3 +44,6 @@ if(canvas){
     canvas.addEventListener("mouseleave", stopPainting);
 }
 
+Array.from(colors).forEach(color => 
+    color.addEventListener("click",handleColorClick)
+    );
